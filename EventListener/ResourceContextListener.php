1f7103d9010fa7ea0912e88dbc6f92f42216c552
vidy <zhengquan.li@remarkdip.com>
2018-12-18 18:23:14 +0800

    require willdurand/negotiation library

--- EventListener/ResourceContextListener.php
+++ EventListener/ResourceContextListener.php
@@ -6,6 +6,7 @@ use Videni\Bundle\RestBundle\Request\RestRequestHeaders;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Videni\Bundle\RestBundle\Config\Resource\ResourceConfigProvider;
 use Videni\Bundle\RestBundle\Operation\ActionTypes;
+use Videni\Bundle\RestBundle\Context\ResourceContextStorage;
 
 class ResourceContextListener
 {
