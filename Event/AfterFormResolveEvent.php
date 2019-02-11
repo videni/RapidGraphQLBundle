2e6832b56a0cc606a4573ddd3823e7dc200d662b
vidy <zhengquan.li@remarkdip.com>
2019-02-11 16:15:41 +0800

    hydrate Symfony form to client with limenius/liform-bundle

---
+++ Event/AfterFormResolveEvent.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace Videni\Bundle\RestBundle\Event;
+
+use Symfony\Component\EventDispatcher\Event;
+use Symfony\Component\Form\FormInterface;
+use Videni\Bundle\RestBundle\Context\ResourceContext;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+
+final class AfterFormResolveEvent extends Event
+{
+    protected $context;
+
+    protected $data;
+
+    protected $response;
+
+    protected $request;
+
+    /**
+     * @var FormInterface
+     */
+    protected $form;
+
+    /**
+     * @param FormInterface $form
+     * @param mixed         $data
+     * @param ResourceContext         $context
+     */
+    public function __construct(FormInterface $form, $data, ResourceContext $context, Request $request)
+    {
+        $this->form = $form;
+        $this->data = $data;
+        $this->context = $context;
+        $this->request = $request;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getContext(): ResourceContext
+    {
+        return $this->context;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getData()
+    {
+        return $this->data;
+    }
+
+    /**
+     * @return FormInterface
+     */
+    public function getForm(): FormInterface
+    {
+        return $this->form;
+    }
+
+    /**
+     * @param mixed $response
+     *
+     * @return self
+     */
+    public function setResponse($response)
+    {
+        $this->response = $response;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getResponse(): ?Response
+    {
+        return $this->response;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getRequest(): Request
+    {
+        return $this->request;
+    }
+}
--- EventListener/FormListener.php
+++ EventListener/FormListener.php
@@ -16,21 +16,36 @@ use Videni\Bundle\RestBundle\Validator\Exception\ValidationException;
 use Videni\Bundle\RestBundle\Context\ResourceContext;
 use Videni\Bundle\RestBundle\Config\Resource\ResourceConfig;
 use Videni\Bundle\RestBundle\Operation\ActionTypes;
+use Limenius\Liform\Liform;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Videni\Bundle\RestBundle\Event\AfterFormResolveEvent;
 
 final class FormListener
 {
+    const AFTER_FORM_RESOLVE = 'videni.form_listener.after_form_resolve';
+
     private $formFactory;
     private $validator;
     private $resourceContextStorage;
+    private $eventDispatcher;
+    private $liform;
 
     public function __construct(
         FormFactoryInterface $formFactory,
         ValidatorInterface $validator,
-        ResourceContextStorage $resourceContextStorage
+        ResourceContextStorage $resourceContextStorage,
+        SerializerInterface $serializer,
+        EventDispatcherInterface $eventDispatcher,
+        Liform $liform
     ) {
         $this->formFactory = $formFactory;
         $this->validator = $validator;
         $this->resourceContextStorage = $resourceContextStorage;
+        $this->serializer = $serializer;
+        $this->eventDispatcher = $eventDispatcher;
+        $this->liform = $liform;
     }
 
     public function onKernelRequest(GetResponseEvent $event)
@@ -48,56 +63,78 @@ final class FormListener
         }
 
         $request = $event->getRequest();
-        if($request->attributes->get('_disable_form', false)) {
+        if ($request->attributes->get('_disable_form', false)) {
             return;
         }
 
-        $form = $this->createForm($context, $request->attributes->get('data'));
-        if ($form) {
-             /**
-             * always use $clearMissing = false, more details in:
-             * @see \VideniBundleRestBundle\Form\FormValidationHandler::validate
-             * @see \VideniBundleRestBundle\Processor\Shared\BuildFormBuilder::$enableFullValidation
-             */
-            $form->submit($this->prepareRequestData($request->request->all()), false);
+        $data = $request->attributes->get('data');
 
-            $violations = $this->validate($form);
-            if (0 !== \count($violations)) {
-                throw new ValidationException($violations);
+        $form = $this->resolveForm($context, $data);
+        $afterFormResolveEvent = new AfterFormResolveEvent($form, $data, $context, $request);
+        $this->eventDispatcher->dispatch(self::AFTER_FORM_RESOLVE, $afterFormResolveEvent);
+        if ($afterFormResolveEvent->getResponse()) {
+            $event->setResponse($afterFormResolveEvent->getResponse());
+
+            return;
+        }
+
+        if (in_array($request->getMethod(), ['POST', 'PUT', 'PATCH'], true)) {
+            /**
+             * always use $clearMissing = false
+             */
+            $isValid = $form->submit($this->prepareRequestData($request->request->all()), false)->isValid();
+            if (false === $isValid) {
+                $data = $this->serializer->normalize($form, null , ['status_code' => Response::HTTP_BAD_REQUEST]) + [
+                    'initial_values' => $form->createView(),
+                    'form_schema' => $this->liform->transform($form),
+                ];
+
+                $this->setResponse($event, $data, Response::HTTP_BAD_REQUEST);
             }
+        }
+        //serialize form and its initial values
+        else {
+            $data = [
+                'initial_values' => $form->createView(),
+                'form_schema' => $this->liform->transform($form)
+            ];
+
+            $this->setResponse($event, $data, Response::HTTP_OK);
         }
     }
 
-    /**
-     * @param FormContext $context
-     */
-    protected function createForm(ResourceContext $context, $data)
+    protected function setResponse($event, $data, $status)
+    {
+        $request = $event->getRequest();
+
+        $event->setResponse(new Response(
+            $this->serializer->serialize($data, $request->getRequestFormat()),
+            $status,
+            [
+                'Content-Type' => sprintf('%s; charset=utf-8', $request->getMimeType($request->getRequestFormat())),
+                'Vary' => 'Accept',
+                'X-Content-Type-Options' => 'nosniff',
+                'X-Frame-Options' => 'deny',
+            ]
+        ));
+    }
+
+    protected function resolveForm(ResourceContext $context, $data)
     {
         $resourceConfig = $context->getResourceConfig();
 
         $formType = $resourceConfig->getOperationAttribute($context->getOperationName(), 'form', true);
         if (null === $formType) {
-             return;
+            return new \LogicException(sprintf('The form is required for operation %s of resource %s', $context->getOperationName(), $context->getClassName()));
         }
 
         $options = [
             'validation_groups' => $resourceConfig->getOperationAttribute($context->getOperationName(), 'validation_groups', true),
             'data_class' =>  $this->getFormDataClass($context, $data),
+            'csrf_protection' => false,
         ];
 
         return $this->formFactory->create($formType, $data, $options);
-    }
-
-    protected function validate(FormInterface $form)
-    {
-        if (!$form->isRoot()) {
-            throw new \InvalidArgumentException('The root form is expected.');
-        }
-        if (!$form->isSubmitted()) {
-            throw new \InvalidArgumentException('The submitted form is expected.');
-        }
-
-        return $this->validator->validate($form);
     }
 
     /**
